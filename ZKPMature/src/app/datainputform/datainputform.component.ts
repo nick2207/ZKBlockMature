c955e8ab733edca58299d89bd9097de33433be7f
Mattia <>
2024-07-21 12:29:41 +0200

    added frontend project

---
+++ ZKPMature/src/app/datainputform/datainputform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datainputform',
+  standalone: true,
+  imports: [],
+  templateUrl: './datainputform.component.html',
+  styleUrl: './datainputform.component.css'
+})
+export class DatainputformComponent {
+
+}
