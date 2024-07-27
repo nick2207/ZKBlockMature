d30f0d160b08b22e5c66362cb8107e4e61268753
nycode <nico.m2207@gmail.com>
2024-07-27 18:40:48 +0200

    [frontend] proof generation ok

--- ZKPMature/src/app/datainputform/datainputform.component.ts
+++ ZKPMature/src/app/datainputform/datainputform.component.ts
@@ -6,6 +6,7 @@ import {
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { groth16 } from 'snarkjs';
 
 @Component({
   selector: 'app-datainputform',
@@ -18,34 +19,30 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 })
 export class DatainputformComponent {
   selectedDate: Date;
-  snarkJSScript: HTMLScriptElement;
-
-  inputJson = {
-    birthYear: '',
-    birthMonth: '',
-    birthDay: '',
-    currentYear: '',
-    currentMonth: '',
-    currentDay: '',
-  };
 
   constructor() {
     this.selectedDate = new Date();
-    this.snarkJSScript = document.createElement('script');
-    this.snarkJSScript.src = 'snarkjs.min.js';
-    document.body.appendChild(this.snarkJSScript);
   }
 
   onSubmit() {
     let currentDate = new Date();
-    this.inputJson.birthYear = this.selectedDate.getFullYear().toString();
-    this.inputJson.birthMonth = (this.selectedDate.getMonth() + 1).toString();
-    this.inputJson.birthDay = this.selectedDate.getDate().toString();
-    this.inputJson.currentDay = currentDate.getDate().toString();
-    this.inputJson.currentMonth = (currentDate.getMonth() + 1).toString();
-    this.inputJson.currentYear = currentDate.getFullYear().toString();
-    let foo = (window as any).snarkjs.groth16.fullProve();
-    console.log(foo);
+    groth16
+      .fullProve(
+        {
+          birthYear: this.selectedDate.getFullYear().toString(),
+          birthMonth: (this.selectedDate.getMonth() + 1).toString(),
+          birthDay: this.selectedDate.getDate().toString(),
+          currentDay: currentDate.getDate().toString(),
+          currentMonth: (currentDate.getMonth() + 1).toString(),
+          currentYear: currentDate.getFullYear().toString(),
+        },
+        'checkAge.wasm',
+        'checkAge_0001.zkey'
+      )
+      .then((data) => {
+        console.log(data.proof);
+        console.log(data.publicSignals);
+      });
   }
 
   onDateChange(event: MatDatepickerInputEvent<Date>): void {
@@ -54,22 +51,4 @@ export class DatainputformComponent {
     }
     this.selectedDate = event.value;
   }
-
-  // async calculateProof() {
-  //   const { proof, publicSignals } = await window.snarkjs.groth16.fullProve(
-  //     { a: 3, b: 11 },
-  //     'circuit.wasm',
-  //     'circuit_final.zkey'
-  //   );
-
-  // proofComponent.innerHTML = JSON.stringify(proof, null, 1);
-
-  // const vkey = await fetch('verification_key.json').then(function (res) {
-  //   return res.json();
-  // });
-
-  // const res = await snarkjs.groth16.verify(vkey, publicSignals, proof);
-
-  // resultComponent.innerHTML = res;
-  // }
 }
