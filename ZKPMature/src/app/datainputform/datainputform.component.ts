b52b237558098165c8a88de4ec4561989932696e
nycode <nico.m2207@gmail.com>
2024-07-27 10:25:14 +0200

    [frontend] can call the snarksjs script

--- ZKPMature/src/app/datainputform/datainputform.component.ts
+++ ZKPMature/src/app/datainputform/datainputform.component.ts
@@ -1,8 +1,11 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatDatepickerInputEvent, MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  MatDatepickerInputEvent,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-datainputform',
@@ -11,27 +14,26 @@ import {provideNativeDateAdapter} from '@angular/material/core';
   providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './datainputform.component.html',
-  styleUrl: './datainputform.component.css'
+  styleUrl: './datainputform.component.css',
 })
 export class DatainputformComponent {
-
   selectedDate: Date;
   snarkJSScript: HTMLScriptElement;
 
   inputJson = {
-    birthYear: "",
-    birthMonth: "",
-    birthDay: "",
-    currentYear: "",
-    currentMonth: "",
-    currentDay: ""
-  }
+    birthYear: '',
+    birthMonth: '',
+    birthDay: '',
+    currentYear: '',
+    currentMonth: '',
+    currentDay: '',
+  };
 
-  constructor () {
+  constructor() {
     this.selectedDate = new Date();
-    this.snarkJSScript = document.createElement("script");
-      this.snarkJSScript.src = "";
-      document.body.appendChild(this.snarkJSScript);
+    this.snarkJSScript = document.createElement('script');
+    this.snarkJSScript.src = 'snarkjs.min.js';
+    document.body.appendChild(this.snarkJSScript);
   }
 
   onSubmit() {
@@ -42,13 +44,32 @@ export class DatainputformComponent {
     this.inputJson.currentDay = currentDate.getDate().toString();
     this.inputJson.currentMonth = (currentDate.getMonth() + 1).toString();
     this.inputJson.currentYear = currentDate.getFullYear().toString();
-
-    
+    let foo = (window as any).snarkjs.groth16.fullProve();
+    console.log(foo);
   }
 
   onDateChange(event: MatDatepickerInputEvent<Date>): void {
-    if(event.value === null) { return; }
+    if (event.value === null) {
+      return;
+    }
     this.selectedDate = event.value;
   }
 
+  // async calculateProof() {
+  //   const { proof, publicSignals } = await window.snarkjs.groth16.fullProve(
+  //     { a: 3, b: 11 },
+  //     'circuit.wasm',
+  //     'circuit_final.zkey'
+  //   );
+
+  // proofComponent.innerHTML = JSON.stringify(proof, null, 1);
+
+  // const vkey = await fetch('verification_key.json').then(function (res) {
+  //   return res.json();
+  // });
+
+  // const res = await snarkjs.groth16.verify(vkey, publicSignals, proof);
+
+  // resultComponent.innerHTML = res;
+  // }
 }
