24af1fb1a5d2e1e2f92160f2b0c843b3ff43c486
nycode <nico.m2207@gmail.com>
2024-09-14 09:46:08 +0200

    [backend] udpated code with the store and verify from a commitment

--- backend/contracts/PedersenCommitment.sol
+++ backend/contracts/PedersenCommitment.sol
@@ -7,26 +7,37 @@ import "./EllipticCurve.sol";
 contract PedersenCommitment is EllipticCurve {
     event LogSender(address indexed sender);
     event LogVal(uint256 indexed sender);
+    event LogCommitment(uint256 indexed txId, uint256 x, uint256 y);
 
-    uint256 public SenderRandomPoint;
+    uint256 public GeneratorRandomPoint;
+
+    mapping(uint256 => Commit) public commitments;
+
+    struct Commit {
+        uint256 x;
+        uint256 y;
+    }
 
     //ensure set at most one time
     modifier isPointSet() {
-        require(SenderRandomPoint == 0, "Cannot set point more than once");
+        require(GeneratorRandomPoint == 0, "Cannot set the generator more than once");
         _;
     }
 
-    function setPoint() public isPointSet {
+    function setPoint() internal isPointSet {
         uint256 randomValue = uint256(
             keccak256(abi.encodePacked(block.timestamp, msg.sender))
         );
-        SenderRandomPoint = randomValue % mod;
+        GeneratorRandomPoint = randomValue % mod;
         emit LogSender(msg.sender);
     }
 
+    /**
+     * probabielmten è da rimuvoere perché getter e setter vengono messi di default
+     */
     function getPoint() public returns (uint256) {
-        emit LogVal(SenderRandomPoint);
-        return SenderRandomPoint;
+        emit LogVal(GeneratorRandomPoint);
+        return GeneratorRandomPoint;
     }
 
     function commit(
@@ -46,7 +57,7 @@ contract PedersenCommitment is EllipticCurve {
         // uint256 y2;
 
         (uint256 senderRandomX, uint256 senderRandomY) = ellipticMul(
-            SenderRandomPoint,
+            GeneratorRandomPoint,
             generatorX,
             generatorY
         );
@@ -61,19 +72,28 @@ contract PedersenCommitment is EllipticCurve {
             generatorY
         );
         (uint256 x, uint256 y) = ellipticAdd(x1, y1, x2, y2);
+
+        uint256 txId = uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender)));
+        commitments[txId] = Commit(x, y);
+
+        emit LogCommitment(txId, x, y);
         return (x, y);
     }
 
     function verify(
         uint256 randomValue,
         uint256 value,
-        uint256 x,
-        uint256 y
+        uint256 txId
     ) public returns (bool) {
+        require(GeneratorRandomPoint != 0, "Generator is not set"); 
+        Commit memory storedCommit = commitments[txId];
+        require(storedCommit.x != 0 || storedCommit.y != 0, "No commitment found for this transaction ID");
+
+
         uint256 xCheck;
         uint256 yCheck;
         (xCheck, yCheck) = commit(randomValue, value);
-        if ((xCheck == x) && (yCheck == y)) {
+        if ((xCheck == storedCommit.x) && (yCheck == storedCommit.y)) {
             return true;
         } else {
             return false;
@@ -110,7 +130,7 @@ contract PedersenCommitment is EllipticCurve {
         uint256 x3;
         uint256 y3;
 
-        if (randomValue1 < randomValue1) {
+        if (randomValue1 < randomValue2) {
             randomValue3 = mod - (randomValue2 - randomValue1);
         } else {
             randomValue3 = (randomValue1 - randomValue2) % mod;
